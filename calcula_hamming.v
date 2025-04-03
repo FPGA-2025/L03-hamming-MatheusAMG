26737f1c4092eb27165bd890f927921c75b6d68a
Matheus <matheusamg@gmail.com>
2025-04-03 20:46:07 -0300

    primeiro teste

--- calcula_hamming.v
+++ calcula_hamming.v
@@ -3,6 +3,14 @@ module calcula_hamming (
   output [14:0] saida
 );
 
-// implemente o seu código aqui
+assign saida[12] = entrada[10];
+assign saida[10:8] = entrada[9:7];
+assign saida[6:0] = entrada[6:0];
+
+
+assign saida[14] = entrada[10] ^ entrada[9] ^ entrada[7] ^ entrada[6] ^ entrada[4] ^ entrada[2] ^ entrada[0]; //p1
+assign saida[13] = entrada[10] ^ entrada[8] ^ entrada[7] ^ entrada[5] ^ entrada[4] ^ entrada[1] ^ entrada[0]; //p2
+assign saida[11] =  entrada[9] ^ entrada[8] ^ entrada[7] ^ entrada[3] ^ entrada[2] ^ entrada[1] ^ entrada[0]; //p3
+assign saida[7] =   entrada[6] ^ entrada[5] ^ entrada[4] ^ entrada[3] ^ entrada[2] ^ entrada[1] ^ entrada[0]; //p8
 
 endmodule
--- corrige_hamming.v
+++ corrige_hamming.v
@@ -1,9 +1,24 @@
 module corrige_hamming (
-  input [14:0] entrada, // a paridade é o bit mais significativo (dado[8])
+  input [14:0] entrada, // a paridade é o bit mais significativo 
   output reg [10:0] saida
 );
 
+reg [3:0] posicao_erro;
+reg [14:0] corrigido;
 
-// implemente o seu código aqui
+// se bit 14, 13, 11 ou 7 ativo, há erro
+always @(*) begin
+  if (entrada[14] || entrada[13] || entrada[11] || entrada[7]) begin
+
+    posicao_erro = {entrada[14], entrada[13], entrada[11], entrada[7]};
+    corrigido = entrada ^ (1 << posicao_erro); // Inverter bit errado
+  end
+end
+
+
+// saida apenas os bits de dados
+always @(*) begin
+  saida = {corrigido[12], corrigido[10:8], corrigido[6:0]}
+end
 
 endmodule
