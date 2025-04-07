067387d2728725ddc55d5370df08b547e4e83658
Matheus <matheusamg@gmail.com>
2025-04-07 20:00:57 -0300

    teste final

--- calcula_hamming.v
+++ calcula_hamming.v
@@ -3,14 +3,14 @@ module calcula_hamming (
   output [14:0] saida
 );
 
-assign saida[12] = entrada[10];
-assign saida[10:8] = entrada[9:7];
-assign saida[6:0] = entrada[6:0];
+assign saida[14:8] = entrada[10:4];
+assign saida[6:4] =  entrada[3:1];
+assign saida[2] =    entrada[0];
 
 
-assign saida[14] = entrada[10] ^ entrada[9] ^ entrada[7] ^ entrada[6] ^ entrada[4] ^ entrada[2] ^ entrada[0]; //p1
-assign saida[13] = entrada[10] ^ entrada[8] ^ entrada[7] ^ entrada[5] ^ entrada[4] ^ entrada[1] ^ entrada[0]; //p2
-assign saida[11] =  entrada[9] ^ entrada[8] ^ entrada[7] ^ entrada[3] ^ entrada[2] ^ entrada[1] ^ entrada[0]; //p3
-assign saida[7] =   entrada[6] ^ entrada[5] ^ entrada[4] ^ entrada[3] ^ entrada[2] ^ entrada[1] ^ entrada[0]; //p8
+assign saida[0] = entrada[0] ^ entrada[1] ^ entrada[3] ^ entrada[4] ^ entrada[6] ^ entrada[8] ^ entrada[10]; //p1
+assign saida[1] = entrada[0] ^ entrada[2] ^ entrada[3] ^ entrada[5] ^ entrada[6] ^ entrada[9] ^ entrada[10]; //p2
+assign saida[3] = entrada[1] ^ entrada[2] ^ entrada[3] ^ entrada[7] ^ entrada[8] ^ entrada[9] ^ entrada[10]; //p3
+assign saida[7] =  ^entrada[10:4]; //p8
 
 endmodule
--- corrige_hamming.v
+++ corrige_hamming.v
@@ -4,60 +4,21 @@ module corrige_hamming (
 );
 
 wire [3:0] posicao_erro;
+wire [3:0] erro_exato;
 reg [14:0] corrigido;
 
-assign posicao_erro[0] = entrada[14] ^ entrada[12] ^ entrada[10] ^ entrada[8] ^ entrada[6] ^ entrada[4] ^ entrada[2] ^ entrada[0];
-assign posicao_erro[1] = entrada[13] ^ entrada[12] ^ entrada[9]  ^ entrada[8] ^ entrada[5] ^ entrada[4] ^ entrada[1] ^ entrada[0];
-assign posicao_erro[2] = entrada[11] ^ entrada[10] ^ entrada[9] ^  entrada[8] ^ entrada[3] ^ entrada[2] ^ entrada[1] ^ entrada[0];
-assign posicao_erro[3] = entrada[7]  ^ entrada[6] ^  entrada[5] ^  entrada[4] ^ entrada[3] ^ entrada[2] ^ entrada[1] ^ entrada[0];
+assign posicao_erro[0] = entrada[0] ^ entrada[2] ^ entrada[4] ^ entrada[6] ^ entrada[8] ^ entrada[10] ^ entrada[12] ^ entrada[14];
+assign posicao_erro[1] = entrada[1] ^ entrada[2] ^ entrada[5] ^ entrada[6] ^ entrada[9] ^ entrada[10] ^ entrada[13] ^ entrada[14];
+assign posicao_erro[2] = entrada[3] ^ entrada[4] ^ entrada[5] ^ entrada[6] ^ entrada[11] ^ entrada[12] ^entrada[13] ^ entrada[14];
+assign posicao_erro[3] = ^entrada[14:7];
 
-// se bit 14, 13, 11 ou 7 ativo, há erro
+assign erro_exato = posicao_erro - 1;
+
+// se posicao_erro > 0 
 always @(*) begin
   corrigido = entrada;
-  if (posicao_erro == 4'b0001) begin
-    corrigido[14] = ~corrigido[14]; // Inverter bit errado
-  end
-  else if(posicao_erro == 4'b0010)begin
-    corrigido[13] = ~corrigido[13];
-  end
-  else if(posicao_erro == 4'b0011)begin
-    corrigido[12] = ~corrigido[12];
-  end
-  else if(posicao_erro == 4'b0100)begin
-    corrigido[11] = ~corrigido[11];
-  end
-  else if(posicao_erro == 4'b0101)begin
-    corrigido[10] = ~corrigido[10];
-  end
-  else if(posicao_erro == 4'b0110)begin
-    corrigido[9] = ~corrigido[9];
-  end
-  else if(posicao_erro == 4'b0111)begin
-    corrigido[8] = ~corrigido[8];
-  end
-  else if(posicao_erro == 4'b1000)begin
-    corrigido[7] = ~corrigido[7];
-  end
-  else if(posicao_erro == 4'b1001)begin
-    corrigido[6] = ~corrigido[6];
-  end
-  else if(posicao_erro == 4'b1010)begin
-    corrigido[5] = ~corrigido[5];
-  end
-  else if(posicao_erro == 4'b1011)begin
-    corrigido[4] = ~corrigido[4];
-  end
-  else if(posicao_erro == 4'b1100)begin
-    corrigido[3] = ~corrigido[3];
-  end
-  else if(posicao_erro == 4'b1101)begin
-    corrigido[2] = ~corrigido[2];
-  end
-  else if(posicao_erro == 4'b1110)begin
-    corrigido[1] = ~corrigido[1];
-  end
-  else if(posicao_erro == 4'b1111)begin
-    corrigido[0] = ~corrigido[0];
+  if (posicao_erro != 0) begin
+    corrigido[erro_exato] = ~entrada[erro_exato];
   end
 end
 
@@ -65,17 +26,17 @@ end
 // saida apenas os bits de dados
 always @(*) begin
   saida = {
+    corrigido[14],      
+    corrigido[13],      
     corrigido[12],      
+    corrigido[11],      
     corrigido[10],      
     corrigido[9],      
     corrigido[8],      
-    corrigido[6],      
-    corrigido[5],      
-    corrigido[4],      
-    corrigido[3],
-    corrigido[2],
-    corrigido[1],
-    corrigido[0]};
+    corrigido[6],
+    corrigido[5],
+    corrigido[4],
+    corrigido[2]};
 end
 
 endmodule
